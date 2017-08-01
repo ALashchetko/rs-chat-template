be3d2c64f4030c435a030169577534c1a891d366
ALashchetko <flatron_vi@mail.ru>
2017-08-01 23:13:35 +0300

    rs-chat-template/add : complete task

--- client/index.js
+++ client/index.js
@@ -1,3 +1,35 @@
 import './index.css';
 
-console.log('Hello world');
+var socket = new WebSocket("ws://localhost:8081");
+
+socket.onopen = function() {
+    alert("Соединение установлено");
+};
+
+document.forms.publish.onsubmit = function() {
+    const outgoingMessage = document.getElementById('userName').innerText + ': ' + this.message.value;
+    socket.send(outgoingMessage);
+    return false;
+};
+
+socket.onmessage = function(event) {
+    const incomingMessage = JSON.parse(event.data);
+    if (incomingMessage.type === 'message') showMessage(incomingMessage.message);
+    else if (incomingMessage.type === 'id') setMyId(incomingMessage.message);
+    else if (incomingMessage.type === 'online') setOnline(incomingMessage.message);
+};
+
+function showMessage(message) {
+    const messageElem = document.createElement('span');
+    messageElem.appendChild(document.createTextNode(message));
+    document.getElementById('messagess').appendChild(messageElem);
+    document.getElementById('messagess').appendChild(document.createElement('p'));
+}
+
+function setMyId(id) {
+    document.getElementById('userName').innerText = 'Anon ' + id;
+}
+
+function setOnline(onlNumber) {
+    document.getElementById('online').innerText = 'Online: ' + onlNumber;
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const WebSocketServer = require('uws').Server;
-const { json } = require('body-parser');
-const { info } = require('./helpers/logger');
-const { port, socketPort, staticPath, isProduction, version } = require('./config');
+const {
+    json
+} = require('body-parser');
+const {
+    info
+} = require('./helpers/logger');
+const {
+    port,
+    socketPort,
+    staticPath,
+    isProduction,
+    version
+} = require('./config');
 
 const server = express();
-const WSServer = new WebSocketServer({ port: socketPort });
+const WSServer = new WebSocketServer({
+    port: socketPort
+});
+
+let clients = {};
+let history = [];
+let currentUsersCount = 0;
 
 info(`---------------------CHAT SERVER v${version}---------------------`);
 server.use(express.static(staticPath));
@@ -17,7 +33,45 @@ server.listen(port, () => {
     info(`WebSocket server was started on port ${socketPort}`);
     info('------------------------------------------------------------');
     WSServer.on('connection', (ws) => {
-        info('user connected');
-        ws.on('message', (message) => info('received', message));
+        currentUsersCount += 1;
+        const id = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+        clients[id] = ws;
+        info('user connected, user id = ' + id);
+        clients[id].send(JSON.stringify({
+            type: 'id',
+            message: id,
+        }));
+        for (let i = 0; i < history.length; i++) {
+            clients[id].send(JSON.stringify(history[i]));
+        }
+        clients[id].send(JSON.stringify({
+            type: 'online',
+            message: currentUsersCount,
+        }));
+        ws.on('message', (message) => {
+            history.push({
+                type: 'message',
+                message: message,
+            });
+            info('received', message);
+            for (var key in clients) {
+                clients[key].send(JSON.stringify({
+                    type: 'message',
+                    message: message,
+                }));
+            }
+            for (let i = 0; i < history.length; i++) {
+                info(history[i].id, history[i].message);
+            }
+            clients[id].send(JSON.stringify({
+                type: 'online',
+                message: currentUsersCount,
+            }));
+        });
+        ws.on('close', () => {
+            currentUsersCount -= 1;
+            info('user disconnected');
+            delete clients[id];
+        });
     });
 });
